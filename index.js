2f0af203d613aa5ad93e88090fbed103b81d02c8
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-05 15:50:43 +0600

    single visa get method done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -35,6 +35,12 @@ async function run() {
            const cursor = visaCollection.find();
            const result = await cursor.toArray()
            res.send(result)
+       })
+       app.get('/visas/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query ={_id:new ObjectId(id)};
+        const result = await visaCollection.findOne(query);
+        res.send(result)
        })
 
        app.post('/visas',async(req,res)=>{
