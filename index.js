69d19887e75941038522375770add40265beab4c
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-07 16:12:07 +0600

    delete visa done

--- index.js
+++ index.js
@@ -89,7 +89,13 @@ async function run() {
             const result = await visaCollection.updateOne(filter,newData)
             res.send(result)
         })
-
+          
+        app.delete('/visas/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)};
+            const result= await visaCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
